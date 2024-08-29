3a86815ff9be3f7cdcab528d5c8fce88f907f836
WENDELL DE SOUZA DORTA <wendell.sdorta@senacsp.edu.br>
2024-08-29 11:35:12 -0300

    começei a fazer a parte de cliente

--- cliente/acesso_res.php
+++ cliente/acesso_res.php
@@ -10,12 +10,12 @@
     // verificar se o usuarios etá logado
     if (!isset($_SESSION['login_usuario'])) {
         // se não existir, redirecionar a sessão por segurança
-        header('location: login_reserva.php'); // redirecionar para login
+        header('location: login_cliente.php'); // redirecionar para login
         exit;
     }
     $nome_da_sessao = session_name();
     if (!isset($_SESSION['nome_da_sessao']) or ($_SESSION['nome_da_sessao'] != $nome_da_sessao)) {
         session_destroy();
-        header('location: login_reserva.php');
+        header('location: login_cliente.php');
     }
 ?>
--- menu_login.php
+++ menu_login.php
@@ -10,64 +10,47 @@
     <!-- Link para CSS específico -->
     <link rel="stylesheet" href="css/estilo.css" type="text/css">
 
-    <title> Cowabunga | Login Administrativo</title>
+    <title>Cowabunga | Acesso</title>
 </head>
 
 <body class="fundofixo">
     <main class="container">
         <section>
-            <article>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                        <h1 class="breadcrumb text-secondary text-center">Faça seu login</h1>
-                        <div class="thumbnail">
-                            <p class="text-secondary text-center" role="alert">
-                                <i class="fas fa-users fa-10x"></i>
-                            </p>
+            <article class="d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
+                <div class="col-xs-12 col-sm-8 col-md-6 text-center">
+                    <h1 class="breadcrumb text-secondary mb-4">Escolha o tipo de login</h1>
+                    <div class="thumbnail">
+                        <p class="text-secondary text-center" role="alert">
+                            <i class="fas fa-users fa-10x"></i>
+                        </p>
+                        <br>
+                        <div class="alert alert-secondary" role="alert">
+                            <!-- Divs para botões com mesmo tamanho e espaçamento -->
+                            <div class="mb-3">
+                                <button class="btn btn-primary btn-lg btn-block" onclick="window.location.href='admin/login.php'">Login
+                                    Administrativo</button>
+                            </div>
                             <br>
-                            <div class="alert alert-secondary" role="alert">
-                                <form action="login.php" name="form_login" id="form_login" method="POST"
-                                    enctype="multipart/form-data">
-                                    <label for="nome_usuario">Email:</label>
-                                    <p class="input-group">
-                                        <span class="input-group-addon">
-                                            <span class="glyphicon glyphicon-user text-secondary" aria-hidden="true"></span>
-                                        </span>
-                                        <input type="text" name="email" id="login" class="form-control" autofocus
-                                            required autocomplete="off" placeholder="Digite seu email">
-                                    </p>
-                                    <label for="senha">Senha:</label>
-                                    <p class="input-group">
-                                        <span class="input-group-addon">
-                                            <span class="glyphicon glyphicon-qrcode text-secondary"
-                                                aria-hidden="true"></span>
-                                        </span>
-                                        <input type="password" name="senha" id="senha" class="form-control" required
-                                            autocomplete="off" placeholder="Digite sua senha">
-                                    </p>
-                                    <p class="text-right">
-                                        <input type="submit" value="Entrar" class="btn btn-secondary">
-                                    </p>
-                                </form>
-                                <p class="text-center">
-                                    <small>
-                                        <br>
-                                        Caso não faça uma escolha em 30 segundos será redirecionado automaticamente para
-                                        página inicial.
-                                    </small>
-                                </p>
-                            </div><!-- fecha alert -->
-                        </div><!-- fecha thumbnail -->
-                    </div><!-- fecha dimensionamento -->
-                </div><!-- fecha row -->
+                            <div>
+                                <button class="btn btn-primary btn-lg btn-block"
+                                    onclick="window.location.href='cliente/login_cliente.php'">Login Cliente</button>
+                            </div>
+                            <p class="text-center mt-3">
+                                <small>
+                                    Caso não faça uma escolha em 30 segundos será redirecionado automaticamente para a
+                                    página inicial.
+                                </small>
+                            </p>
+                        </div><!-- fecha alert -->
+                    </div><!-- fecha thumbnail -->
+                </div><!-- fecha col-md-6 -->
             </article>
         </section>
     </main>
 
-
     <!-- Link arquivos Bootstrap js -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
